c527896a46343cfaf3907bb59955b2de39ea5326
awissing <atwissing@pipeline.sbcc.edu>
2016-11-13 16:44:41 -0800

    Update Hand.cpp
    new functions for hand

--- src/Hand.cpp
+++ src/Hand.cpp
@@ -21,6 +21,14 @@ using namespace std;
 
 Hand::Hand(){
 	this->HandRank = 0;
+	
+	//The follow is just for testing and will be replaced by dealing from the deck
+	for (int i=0; i<2;i++){
+		hand.push_back(Card(make_pair(1,1)));
+	}
+	for (int i=0; i<2;i++){
+		hand.push_back(Card(make_pair(2,2)));
+	}
 }
 
 
@@ -29,6 +37,8 @@ Hand::Hand(int HandRank, vector<Card> hand) {
 
 	this->HandRank = HandRank;
 	this->hand = hand;
+	
+
 
 }
 
@@ -44,3 +54,47 @@ map<int,int> Hand::countMatches(){
 	}
 	return matches;
 }
+
+vector <int> Hand::countMatchesVector(){
+	vector <int> count(13,0);
+	for(unsigned int i = 0; i < hand.size(); i++){
+		count[hand[i].Value]++;
+	}
+	return count;
+}
+
+int Hand::getMaxMatches(){
+	int max;
+	for(unsigned int i = 0; i < matches.size(); i++){
+		int n = matches[i];
+		if ( n > max)
+			max = n;
+	}
+	return max;
+}
+
+bool Hand::has1pair(){
+	if (getMaxMatches() >= 2 )
+		return true;
+	return false;
+}
+
+/*
+there is a run time error that needs to be fixed, but the function will do the following:
+	if there is a pair in the hand:
+		remove the pair
+		if there is another pair remaining:
+			return true
+*/
+bool Hand::has2pairs(){
+	if(has1pair()){
+		vector <int> matches = countMatchesVector();
+		//remove the pair:
+		matches.erase( std::remove(matches.begin(), matches.end(), 2) , matches.end() );
+		//if another pair is found, then there were two pairs:
+		if(std::find(matches.begin(), matches.end(), 2) != matches.end())
+			return true;
+	}
+	return false;
+}
+
